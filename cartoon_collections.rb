e02512a7f8b297d4c238b885960f2aeb8b0f0138
Aniket B <aniket.b@outlook.com>
2019-09-08 19:14:18 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,11 +7,18 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
+<<<<<<< HEAD
   if array.length > 4
     return false
   else
     return true
   end
+=======
+  if array.size > 4
+     true
+  else
+     false
+>>>>>>> 94f77d4052847835b244aa29555f6819de1ea7b9
 end
 
 def find_the_cheese(array)
